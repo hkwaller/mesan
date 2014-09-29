79bf31d51373527539bb64d182f84bedecbf39a7
henrikholmsen <Henrik@Henriks-Macbook.local>
2014-09-29 14:38:09 +0200

    Added dish class

---
+++ Mesanius/Mesanius/Dish.swift
@@ -0,0 +1,24 @@
+//
+//  Dish.swift
+//  Mesanius
+//
+//  Created by Henrik Holmsen on 29.09.14.
+//  Copyright (c) 2014 Hannes Waller. All rights reserved.
+//
+
+import Foundation
+
+class Dish {
+    
+    var id: Int
+    var name: String
+    var price: Int
+    
+    init(id: Int, name: String, price: Int){
+    
+        self.id = id
+        self.name = name
+        self.price = price
+        
+    }
+}
