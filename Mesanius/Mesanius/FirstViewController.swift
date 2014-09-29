5e3ef43e20cfe569326290997074a5a31b53cf14
hkwaller <hkwaller@gmail.com>
2014-09-29 13:42:25 +0200

    json

--- Mesanius/Mesanius/FirstViewController.swift
+++ Mesanius/Mesanius/FirstViewController.swift
@@ -12,7 +12,43 @@ class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let urlPath = "http://localhost:8080/rest/menu/"
+        let url = NSURL(string: urlPath)
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithURL(url, completionHandler: {data, response, error -> Void in
+            
+            if (error != nil) {
+                println(error)
+            } else {
+                let jsonResult: AnyObject? = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil)
+                println(jsonResult)
+                
+                if let dict = jsonResult as? NSArray {
+                    for food : AnyObject in dict {
+                        if let foodInfo = food as? Dictionary<String, AnyObject> {
+                            let id : AnyObject = foodInfo["id"]!
+                            let name : AnyObject = foodInfo["name"]!
+                            //let description : AnyObject? = foodInfo["description"]
+                            let price : AnyObject = foodInfo["price"]!
+                            
+                            println("ID: \(id)")
+                            println("Name: \(name)")
+                            //println("Description: \(description)")
+                            println("Price: \(price)")
+                            println("")
+                        }
+                    }
+                    
+                    
+                    
+                }
+                
+            }
+        })
+        
+        task.resume()
+    
     }
 
     override func didReceiveMemoryWarning() {
