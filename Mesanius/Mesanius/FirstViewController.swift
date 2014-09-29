23f0e351d7433447c696a9be63a48d3f530ad138
hkwaller <hkwaller@gmail.com>
2014-09-29 13:27:17 +0200

    initial commit

---
+++ Mesanius/Mesanius/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Mesanius
+//
+//  Created by Hannes Waller on 2014-09-29.
+//  Copyright (c) 2014 Hannes Waller. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
